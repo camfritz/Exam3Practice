c61920f3cf94fc80260971d6bba9a3df9313bcd2
Camron Fritz <camfritz@gmail.com>
2018-12-03 11:19:13 -0800

    Add email sending of mangled name.

--- app/src/main/java/edu/fritzcamoregonstate/exampractice3/MangleNiceActivity.java
+++ app/src/main/java/edu/fritzcamoregonstate/exampractice3/MangleNiceActivity.java
@@ -2,6 +2,7 @@ package edu.fritzcamoregonstate.exampractice3;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -39,7 +40,7 @@ public class MangleNiceActivity extends AppCompatActivity {
         //Set button views
         mRemangleButton = findViewById(R.id.remangle_button_nice);
         mResetButton = findViewById(R.id.reset_button_nice);
-        mSendButton = findViewById(R.id.reset_button_nice);
+        mSendButton = findViewById(R.id.send_mangle_nice);
         mPhotoButton = findViewById(R.id.add_photo_nice);
 
         mNameNice = findViewById(R.id.name_nice);
@@ -60,6 +61,15 @@ public class MangleNiceActivity extends AppCompatActivity {
             public void onClick(View v) {
                 ResetDialogFragment resetDialog = new ResetDialogFragment();
                 resetDialog.show(fragmentManager, DIALOG_RESET);
+            }
+        });
+
+        mSendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mailIntent = new Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:"));
+                mailIntent.putExtra(Intent.EXTRA_TEXT, mName);
+                startActivity(Intent.createChooser(mailIntent, "Send Name"));
             }
         });
     }
--- app/src/main/java/edu/fritzcamoregonstate/exampractice3/MangleRudeActivity.java
+++ app/src/main/java/edu/fritzcamoregonstate/exampractice3/MangleRudeActivity.java
@@ -2,6 +2,7 @@ package edu.fritzcamoregonstate.exampractice3;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -39,7 +40,7 @@ public class MangleRudeActivity extends AppCompatActivity {
         //Set button views
         mRemangleButton = findViewById(R.id.remangle_button_rude);
         mResetButton = findViewById(R.id.reset_button_rude);
-        mSendButton = findViewById(R.id.reset_button_rude);
+        mSendButton = findViewById(R.id.send_mangle_rude);
         mPhotoButton = findViewById(R.id.add_photo_rude);
 
         mNameRude = findViewById(R.id.name_rude);
@@ -60,6 +61,15 @@ public class MangleRudeActivity extends AppCompatActivity {
             public void onClick(View v) {
                 ResetDialogFragment resetDialog = new ResetDialogFragment();
                 resetDialog.show(fragmentManager, DIALOG_RESET);
+            }
+        });
+
+        mSendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mailIntent = new Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:"));
+                mailIntent.putExtra(Intent.EXTRA_TEXT, mName);
+                startActivity(Intent.createChooser(mailIntent, "Send Name"));
             }
         });
     }
